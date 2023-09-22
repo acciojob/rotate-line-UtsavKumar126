08832ccc1bfca4961bbc6008dcfb34ff35ba8a3c
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-22 23:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-//your JS code here. If required.
-const div=document.getElementById("line");
-changeDegree(){
-	div.style.transform="rotate(2deg)"
-}
+  const line = document.getElementById('line');
+  
+  // Initial angle
+  let angle = 0;
+
+  // Function to update the rotation
+  function rotateLine() {
+    // Update the rotation angle
+    angle += 2;
+
+    // Apply the rotation transform to the line
+    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+
+    // Call the function again in 20 milliseconds for smooth animation
+    setTimeout(rotateLine, 20);
+  }
+
+  // Start the rotation
+  rotateLine();
